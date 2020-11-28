c84c6a2838a184db9509054d192dff92b7194de3
Ichirou2910 <ichiroukeita2910@gmail.com>
2020-11-28 14:38:07 +0700

    Resize font

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use('/api/comment', require('./routes/comment.route'));
 app.use('/api/image', require('./routes/image.route'));
 
 app.use((req, res, next) => {
-	res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
 app.use((error, req, res, next) => {
@@ -36,6 +36,19 @@ app.use((error, req, res, next) => {
 	}
 	res.status(error.code || 500);
 	res.json({ message: error.message || 'An error occured!' });
+});
+
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	);
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, PUT, PATCH, DELETE'
+	);
+	next();
 });
 
 const PORT = process.env.PORT || 5000;
